c01cfd4c71b578456b1941013b663a950b1f14f9
SymphonyDawn3 <symphonydawn3@gmail.com>
2025-10-24 19:15:04 +0800

    Add friendship bow on Flawless's hat if tamed

--- src/main/java/org/projectflawless/minelittleflawless/client/renderer/FlawlessRenderer.java
+++ src/main/java/org/projectflawless/minelittleflawless/client/renderer/FlawlessRenderer.java
@@ -1,13 +1,23 @@
 package org.projectflawless.minelittleflawless.client.renderer;
 
+import org.projectflawless.minelittleflawless.procedures.IfFlawlessIsTamedProcedure;
 import org.projectflawless.minelittleflawless.entity.FlawlessEntity;
+import org.projectflawless.minelittleflawless.client.model.ModelFriendshipBow;
 import org.projectflawless.minelittleflawless.client.model.ModelFlawless;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.Minecraft;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 public class FlawlessRenderer extends MobRenderer<FlawlessEntity, LivingEntityRenderState, ModelFlawless> {
@@ -15,6 +25,23 @@ public class FlawlessRenderer extends MobRenderer<FlawlessEntity, LivingEntityRe
 
 	public FlawlessRenderer(EntityRendererProvider.Context context) {
 		super(context, new ModelFlawless(context.bakeLayer(ModelFlawless.LAYER_LOCATION)), 0.5f);
+		this.addLayer(new RenderLayer<>(this) {
+			final ResourceLocation LAYER_TEXTURE = ResourceLocation.parse("minelittleflawless:textures/entities/friendship_bow.png");
+
+			@Override
+			public void render(PoseStack poseStack, MultiBufferSource bufferSource, int light, LivingEntityRenderState state, float headYaw, float headPitch) {
+				Level world = entity.level();
+				double x = entity.getX();
+				double y = entity.getY();
+				double z = entity.getZ();
+				if (IfFlawlessIsTamedProcedure.execute(entity)) {
+					VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(LAYER_TEXTURE));
+					EntityModel model = new ModelFriendshipBow(Minecraft.getInstance().getEntityModels().bakeLayer(ModelFriendshipBow.LAYER_LOCATION));
+					model.setupAnim(state);
+					model.renderToBuffer(poseStack, vertexConsumer, light, LivingEntityRenderer.getOverlayCoords(state, 0));
+				}
+			}
+		});
 	}
 
 	@Override
