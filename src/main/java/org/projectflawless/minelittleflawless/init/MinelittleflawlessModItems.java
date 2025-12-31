eb928bd3ee4473eaf9ca2b600d5d115b0ceaf214
SymphonyDawn3 <86187686+SymphonyDawn3@users.noreply.github.com>
2025-12-31 13:36:26 +0800

    Changed generic types to specialized Item subclass of Flawless items

--- src/main/java/org/projectflawless/minelittleflawless/init/MinelittleflawlessModItems.java
+++ src/main/java/org/projectflawless/minelittleflawless/init/MinelittleflawlessModItems.java
@@ -13,13 +13,13 @@ import java.util.function.Function;
 
 public class MinelittleflawlessModItems {
     public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(MinelittleflawlessMod.MODID);
-    public static final DeferredItem<Item> FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", properties -> new SpawnEggItem(MinelittleflawlessModEntities.FLAWLESS.get(), properties));
-    public static final DeferredItem<Item> FLAWLESS_MAGICIAN_CLOTHING = register("flawless_magician_clothing", FlawlessMagicianClothingItem::new);
-    public static final DeferredItem<Item> TUXEDO = register("tuxedo", TuxedoItem::new);
-    public static final DeferredItem<Item> FARMER = register("farmer", FarmerItem::new);
-    public static final DeferredItem<Item> PAJAMAS = register("pajamas", PajamasItem::new);
-    public static final DeferredItem<Item> SCHOOLGIRL = register("schoolgirl", SchoolgirlItem::new);
-    public static final DeferredItem<Item> ROCKSTAR = register("rockstar", RockstarItem::new);
+    public static final DeferredItem<SpawnEggItem> FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", properties -> new SpawnEggItem(MinelittleflawlessModEntities.FLAWLESS.get(), properties));
+    public static final DeferredItem<FlawlessMagicianClothingItem> FLAWLESS_MAGICIAN_CLOTHING = register("flawless_magician_clothing", FlawlessMagicianClothingItem::new);
+    public static final DeferredItem<TuxedoItem> TUXEDO = register("tuxedo", TuxedoItem::new);
+    public static final DeferredItem<FarmerItem> FARMER = register("farmer", FarmerItem::new);
+    public static final DeferredItem<PajamasItem> PAJAMAS = register("pajamas", PajamasItem::new);
+    public static final DeferredItem<SchoolgirlItem> SCHOOLGIRL = register("schoolgirl", SchoolgirlItem::new);
+    public static final DeferredItem<RockstarItem> ROCKSTAR = register("rockstar", RockstarItem::new);
 
     private static <I extends Item> DeferredItem<I> register(String name, Function<Item.Properties, ? extends I> supplier) {
         return REGISTRY.registerItem(name, supplier, new Item.Properties());
