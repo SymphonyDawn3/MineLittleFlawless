c449f2aeea3da8300f5c20a63b20191c45342f11
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-01-18 12:05:42 +0800

    Add Twixie spawn eggs and change Flawless spawn egg appearance
    Also change from SpawnEggItem to DeferredSpawnEggItem to remove deprecation warning,

--- src/main/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessItems.java
+++ src/main/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessItems.java
@@ -1,19 +1,21 @@
 package org.projectflawless.minelittleflawless.init;
 
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 import org.projectflawless.minelittleflawless.item.*;
 import org.projectflawless.minelittleflawless.MineLittleFlawless;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredItem;
 
-import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
 
 public class MineLittleFlawlessItems {
     public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(MineLittleFlawless.MODID);
-    public static final DeferredItem<SpawnEggItem> FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", properties -> new SpawnEggItem(MineLittleFlawlessEntities.FLAWLESS.get(), 10205183, 10205183, properties));
+    public static final DeferredItem<DeferredSpawnEggItem> FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", properties -> new DeferredSpawnEggItem(MineLittleFlawlessEntities.FLAWLESS, 10730239, 11181311, properties));
+    public static final DeferredItem<DeferredSpawnEggItem> TWILIGHT_SPAWN_EGG = register("twilight_spawn_egg", properties -> new DeferredSpawnEggItem(MineLittleFlawlessEntities.TWILIGHT, 13409503, 6630791, properties));
+    public static final DeferredItem<DeferredSpawnEggItem> TRIXIE_SPAWN_EGG = register("trixie_spawn_egg", properties -> new DeferredSpawnEggItem(MineLittleFlawlessEntities.TRIXIE, 7123690,14808319, properties));
     public static final DeferredItem<FlawlessMagicianClothingItem> FLAWLESS_MAGICIAN_CLOTHING = register("flawless_magician_clothing", FlawlessMagicianClothingItem::new);
     public static final DeferredItem<TuxedoItem> TUXEDO = register("tuxedo", TuxedoItem::new);
     public static final DeferredItem<FarmerItem> FARMER = register("farmer", FarmerItem::new);
