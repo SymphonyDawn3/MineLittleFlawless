cd2fd674ced9f7db45b7bb8024fc408b83019a7e
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-03-08 21:18:41 +0800

    Add TamersPonyModel to refactor and adjust models to appropriate races

--- src/client/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessModels.java
+++ src/client/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessModels.java
@@ -12,11 +12,12 @@ public class MineLittleFlawlessModels {
 		EntityModelLayerRegistry.registerModelLayer(SchoolgirlModel.LAYER_LOCATION, SchoolgirlModel::createBodyLayer);
 		EntityModelLayerRegistry.registerModelLayer(TuxedoModel.LAYER_LOCATION, TuxedoModel::createBodyLayer);
 		EntityModelLayerRegistry.registerModelLayer(RockstarModel.LAYER_LOCATION, RockstarModel::createBodyLayer);
-		EntityModelLayerRegistry.registerModelLayer(FlawlessModel.LAYER_LOCATION, FlawlessModel::createBodyLayer);
-        EntityModelLayerRegistry.registerModelLayer(TwilightModel.LAYER_LOCATION, TwilightModel::createBodyLayer);
-        EntityModelLayerRegistry.registerModelLayer(TrixieModel.LAYER_LOCATION, TrixieModel::createBodyLayer);
-        EntityModelLayerRegistry.registerModelLayer(ArinosModel.LAYER_LOCATION, ArinosModel::createBodyLayer);
-        EntityModelLayerRegistry.registerModelLayer(TrixiebelleModel.LAYER_LOCATION, TrixiebelleModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(TamersPonyModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
+		EntityModelLayerRegistry.registerModelLayer(FlawlessModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(TwilightModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(TrixieModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(ArinosModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(TrixiebelleModel.LAYER_LOCATION, TamersPonyModel::createBodyLayer);
         EntityModelLayerRegistry.registerModelLayer(JesterModel.LAYER_LOCATION, JesterModel::createBodyLayer);
         EntityModelLayerRegistry.registerModelLayer(TrixiebelleJesterModel.LAYER_LOCATION, TrixiebelleJesterModel::createBodyLayer);
         EntityModelLayerRegistry.registerModelLayer(UnicornHornCoronaModel.LAYER_LOCATION, UnicornHornCoronaModel::createBodyLayer);
