51c4505ddca4b2c517901e41522f180ef8d6f652
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-03-15 13:55:49 +0800

    Change remaining spawn egg colors from integer to hexadecimal

--- src/main/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessItems.java
+++ src/main/java/org/projectflawless/minelittleflawless/init/MineLittleFlawlessItems.java
@@ -12,10 +12,10 @@ import net.minecraft.world.item.Item;
 import java.util.function.Supplier;
 
 public class MineLittleFlawlessItems {
-    public static final SpawnEggItem FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.FLAWLESS, 10730239, 11181311, new Item.Properties()));
-    public static final SpawnEggItem TWILIGHT_SPAWN_EGG = register("twilight_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.TWILIGHT, 13409503, 6630791, new Item.Properties()));
-    public static final SpawnEggItem TRIXIE_SPAWN_EGG = register("trixie_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.TRIXIE, 7123690, 14808319, new Item.Properties()));
-    public static final SpawnEggItem ARINOS_SPAWN_EGG = register("arinos_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.ARINOS, 16636668, 3717372, new Item.Properties()));
+    public static final SpawnEggItem FLAWLESS_SPAWN_EGG = register("flawless_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.FLAWLESS, 0xa3baff, 0xaa9cff, new Item.Properties()));
+    public static final SpawnEggItem TWILIGHT_SPAWN_EGG = register("twilight_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.TWILIGHT, 0xcc9cdf, 0x652d87, new Item.Properties()));
+    public static final SpawnEggItem TRIXIE_SPAWN_EGG = register("trixie_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.TRIXIE, 0x6cb2ea, 0xe1f4ff, new Item.Properties()));
+    public static final SpawnEggItem ARINOS_SPAWN_EGG = register("arinos_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.ARINOS, 0xfddafc, 0x38b8fc, new Item.Properties()));
     public static final SpawnEggItem TRIXIEBELLE_SPAWN_EGG = register("trixiebelle_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.TRIXIEBELLE, 0xfdf0b5, 0xff7d69, new Item.Properties()));
     public static final SpawnEggItem MARIONETTE_SPAWN_EGG = register("marionette_spawn_egg", () -> new SpawnEggItem(MineLittleFlawlessEntities.MARIONETTE, 0xffffff, 0x302631, new Item.Properties()));
     public static final FlawlessMagicianClothingItem FLAWLESS_MAGICIAN_CLOTHING = register("flawless_magician_clothing", () -> new FlawlessMagicianClothingItem(new Item.Properties()));
