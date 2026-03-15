46b99653874aa17ec2addc7105214c91f7f4bd0e
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-03-15 21:02:11 +0800

    Add breeding and taming capabilities to Marionette

--- src/main/java/org/projectflawless/minelittleflawless/entity/Marionette.java
+++ src/main/java/org/projectflawless/minelittleflawless/entity/Marionette.java
@@ -4,6 +4,10 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.projectflawless.minelittleflawless.init.MineLittleFlawlessEntities;
@@ -11,6 +15,12 @@ import org.projectflawless.minelittleflawless.init.MineLittleFlawlessEntities;
 public class Marionette extends TamableTamersPony {
     public Marionette(EntityType<Marionette> type, Level world) {
         super(type, world);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(9, new TemptGoal(this, 1, Ingredient.of(Items.SWEET_BERRIES), false));
     }
 
     @Override
@@ -23,5 +33,10 @@ public class Marionette extends TamableTamersPony {
                     MobSpawnType.BREEDING, null, null);
 
         return babyMarionette;
+    }
+
+    @Override
+    public boolean isFood(ItemStack stack) {
+        return stack.is(Items.SWEET_BERRIES);
     }
 }
