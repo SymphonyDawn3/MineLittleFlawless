b8d0dea1bc8ddda3bd656b10310992d2d6c39c6b
SymphonyDawn3 <86187686+SymphonyDawn3@users.noreply.github.com>
2026-04-21 20:26:50 +0800

    Add the ability to tame Star Catcher

--- src/main/java/org/projectflawless/minelittleflawless/entity/StarCatcher.java
+++ src/main/java/org/projectflawless/minelittleflawless/entity/StarCatcher.java
@@ -1,9 +1,15 @@
 package org.projectflawless.minelittleflawless.entity;
 
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.projectflawless.minelittleflawless.init.MineLittleFlawlessEntities;
@@ -11,6 +17,28 @@ import org.projectflawless.minelittleflawless.init.MineLittleFlawlessEntities;
 public class StarCatcher extends TamableTamersPony {
     public StarCatcher(EntityType<StarCatcher> type, Level world) {
         super(type, world);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(9, new TemptGoal(this, 1, Ingredient.of(Items.HONEY_BOTTLE), false));
+    }
+
+    @Override
+    public boolean canAttackType(EntityType<?> entityType) {
+        return !this.getType().equals(entityType);
+    }
+
+    @Override
+    protected void usePlayerItem(Player player, InteractionHand hand, ItemStack stack) {
+        if (stack.is(Items.HONEY_BOTTLE) && !player.getAbilities().instabuild) {
+            ItemStack glassBottle = new ItemStack(Items.GLASS_BOTTLE);
+            if (!player.getInventory().add(glassBottle)) {
+                player.drop(stack, false);
+            }
+        }
+        super.usePlayerItem(player, hand, stack);
     }
 
     @Override
@@ -22,5 +50,10 @@ public class StarCatcher extends TamableTamersPony {
         }
 
         return retval;
+    }
+
+    @Override
+    public boolean isFood(ItemStack stack) {
+        return stack.is(Items.HONEY_BOTTLE);
     }
 }
