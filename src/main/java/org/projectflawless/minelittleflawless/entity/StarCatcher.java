a4555b7539eb60f3fa0ab76cf1f0b98284010bd0
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-05-03 14:12:11 +0800

    Add Star Catcher picking up and giving items behavior

--- src/main/java/org/projectflawless/minelittleflawless/entity/StarCatcher.java
+++ src/main/java/org/projectflawless/minelittleflawless/entity/StarCatcher.java
@@ -1,22 +1,117 @@
 package org.projectflawless.minelittleflawless.entity;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.behavior.EntityTracker;
+import net.minecraft.world.entity.ai.behavior.PositionTracker;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.npc.InventoryCarrier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.sensor.PredicateSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyPlayersSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearestItemSensor;
+import net.tslat.smartbrainlib.util.BrainUtils;
 import org.jetbrains.annotations.Nullable;
+import org.projectflawless.minelittleflawless.entity.ai.behavior.GoAndThrowItems;
+import org.projectflawless.minelittleflawless.entity.ai.behavior.GoToWantedItem;
 import org.projectflawless.minelittleflawless.init.MineLittleFlawlessEntities;
 
-public class StarCatcher extends TamableTamersPony {
+import java.util.List;
+import java.util.Optional;
+
+public class StarCatcher extends TamableTamersPony implements InventoryCarrier, SmartBrainOwner<StarCatcher> {
+    private static final List<? extends PredicateSensor<?, ? extends StarCatcher>> SENSORS = List.of(
+            new NearestItemSensor<>(),
+            new NearbyPlayersSensor<StarCatcher>()
+                    .setPredicate((player, starCatcher) -> !player.isSpectator() && starCatcher.isOwnedBy(player))
+    );
+
+    private static final BrainActivityGroup<StarCatcher> CORE_TASKS = BrainActivityGroup.coreTasks(
+            new LookAtTarget<>(),
+            new MoveToWalkTarget<>()
+    );
+
+    private static final BrainActivityGroup<StarCatcher> IDLE_TASKS = BrainActivityGroup.idleTasks(
+            new FirstApplicableBehaviour<>(
+                    GoToWantedItem.create(starCatcher -> true, 1.75f, true, 10),
+                    new GoAndThrowItems<>(StarCatcher::trackTarget, 1f)
+            )
+    );
+
+    private final SimpleContainer inventory = new SimpleContainer(3);
     public StarCatcher(EntityType<StarCatcher> type, Level world) {
         super(type, world);
+        this.setCanPickUpLoot(true);
+    }
+
+    @Override
+    public List<? extends PredicateSensor<?, ? extends StarCatcher>> getSensors() {
+        return SENSORS;
+    }
+
+    @Override
+    protected Brain.Provider<StarCatcher> brainProvider() {
+        return new SmartBrainProvider<>(this, true, false);
+    }
+
+    @Override
+    public BrainActivityGroup<StarCatcher> getCoreTasks() {
+        return CORE_TASKS;
+    }
+
+    @Override
+    public BrainActivityGroup<? extends StarCatcher> getIdleTasks() {
+        return IDLE_TASKS;
+    }
+
+    @Override
+    protected void customServerAiStep() {
+        this.tickBrain(this);
+        super.customServerAiStep();
+    }
+
+    @Override
+    public SimpleContainer getInventory() {
+        return this.inventory;
+    }
+
+    @Override
+    public boolean wantsToPickUp(ItemStack stack) {
+        return this.inventory.canAddItem(stack) && this.isTame();
+    }
+
+    @Override
+    protected void pickUpItem(ItemEntity itemEntity) {
+        InventoryCarrier.pickUpItem(this, this, itemEntity);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        this.writeInventoryToTag(compound);
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compound) {
+        super.readAdditionalSaveData(compound);
+        this.readInventoryFromTag(compound);
     }
 
     @Override
@@ -55,5 +150,19 @@ public class StarCatcher extends TamableTamersPony {
     @Override
     public boolean isFood(ItemStack stack) {
         return stack.is(Items.HONEY_BOTTLE);
+    }
+
+    @Override
+    protected void dropCustomDeathLoot(DamageSource damageSource, int looting, boolean hitByPlayer) {
+        for (ItemStack item : this.inventory.items) {
+            this.spawnAtLocation(item);
+        }
+        this.inventory.clearContent();
+    }
+
+    private static Optional<PositionTracker> trackTarget(StarCatcher starCatcher) {
+        return BrainUtils.hasMemory(starCatcher, MemoryModuleType.NEAREST_VISIBLE_PLAYER)
+                ? Optional.of(new EntityTracker(BrainUtils.getMemory(starCatcher, MemoryModuleType.NEAREST_VISIBLE_PLAYER), false))
+                : Optional.empty();
     }
 }
