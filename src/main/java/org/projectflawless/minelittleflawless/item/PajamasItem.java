9ff740fee6368393ebfcb6e1d13ecc161b7937d9
SymphonyDawn3 <symphonydawn3@gmail.com>
2026-05-25 21:39:13 +0800

    Improve Flawless clothing codebase
    Moved `Flawless.getRandomItemFromTags` to `Utils.getRandomItemFromTags` for the general use of getting an item from a tag.
    
    Added `FlawlessClothingItem.clothing` for clothing to be added to her from subclass initialization.
    
    Refactored the remaining codebase for `Flawless`.

--- src/main/java/org/projectflawless/minelittleflawless/item/PajamasItem.java
+++ src/main/java/org/projectflawless/minelittleflawless/item/PajamasItem.java
@@ -1,9 +1,10 @@
 package org.projectflawless.minelittleflawless.item;
 
 import net.minecraft.world.item.Item;
+import org.projectflawless.minelittleflawless.Clothing;
 
 public class PajamasItem extends FlawlessClothingItem {
 	public PajamasItem(Item.Properties properties) {
-		super(properties.stacksTo(1));
+		super(properties.stacksTo(1), Clothing.PAJAMAS);
 	}
 }
