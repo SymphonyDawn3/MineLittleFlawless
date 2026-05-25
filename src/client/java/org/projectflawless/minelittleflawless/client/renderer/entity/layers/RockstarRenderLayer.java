ed1191d16f09252097a871fbd1f9e5fb9dbea40f
SymphonyDawn3 <86187686+SymphonyDawn3@users.noreply.github.com>
2026-05-25 18:09:26 +0800

    Add `Clothing` class for `ResourceLocation`s of Flawless clothing
    Added the `Clothing` class which adds `ResourceLocation`s of Flawless's clothes, that should be independent of the item values themselves.

--- src/client/java/org/projectflawless/minelittleflawless/client/renderer/entity/layers/RockstarRenderLayer.java
+++ src/client/java/org/projectflawless/minelittleflawless/client/renderer/entity/layers/RockstarRenderLayer.java
@@ -1,16 +1,15 @@
 package org.projectflawless.minelittleflawless.client.renderer.entity.layers;
 
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import org.projectflawless.minelittleflawless.Clothing;
 import org.projectflawless.minelittleflawless.MineLittleFlawless;
 import org.projectflawless.minelittleflawless.client.model.entity.clothing.RockstarModel;
 import org.projectflawless.minelittleflawless.client.renderer.entity.FlawlessRenderer;
-import org.projectflawless.minelittleflawless.init.MineLittleFlawlessItems;
 
 public class RockstarRenderLayer extends FlawlessClothingRenderLayer {
     public RockstarRenderLayer(FlawlessRenderer renderer) {
         super(renderer, new ResourceLocation(MineLittleFlawless.MOD_ID, "textures/entities/rockstar.png"),
                 new RockstarModel(entityModels.bakeLayer(RockstarModel.LAYER_LOCATION)),
-                BuiltInRegistries.ITEM.getKey(MineLittleFlawlessItems.ROCKSTAR).toString());
+                Clothing.ROCKSTAR);
     }
 }
