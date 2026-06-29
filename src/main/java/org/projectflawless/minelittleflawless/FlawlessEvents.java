e546aa9aca1a51763d0ef447a336589068686001
SymphonyDawn3 <86187686+SymphonyDawn3@users.noreply.github.com>
2026-06-29 21:04:28 +0800

    Add the ability of tamable ponies to make their enemies drop loot
    Specifically, the enemy will drop items and XP that the player or tamed wolf will have to get otherwise.

--- src/main/java/org/projectflawless/minelittleflawless/FlawlessEvents.java
+++ src/main/java/org/projectflawless/minelittleflawless/FlawlessEvents.java
@@ -1,10 +1,26 @@
 package org.projectflawless.minelittleflawless;
 
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.projectflawless.minelittleflawless.entity.Flawless;
+import org.projectflawless.minelittleflawless.entity.TamableTamersPony;
 
 public class FlawlessEvents {
     public static void init() {
+        // This allows farmer Flawless to give you items whenever you go to bed.
         EntitySleepEvents.ALLOW_SLEEPING.register(Flawless::whenPlayerWakesUp);
+
+        // This allows tamed ponies to make their enemies drop XP and items otherwise only obtainable only by players
+        // and tamed wolves.
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register((entity, source, amount) -> {
+            Entity entity1 = source.getEntity();
+            if (entity1 instanceof TamableTamersPony tamersPony && tamersPony.isTame()) {
+                if (tamersPony.getOwner() instanceof Player player)
+                    entity.setLastHurtByPlayer(player);
+            }
+            return true;
+        });
     }
 }
