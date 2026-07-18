228eb18d586067700e3670470e9cb95a2220fa72
SymphonyDawn3 <86187686+SymphonyDawn3@users.noreply.github.com>
2026-07-18 11:35:50 +0800

    Add raid handling capabilities for tamed ponies
    If a tamed pony kills a pillager captain, it will give the player the Bad Omen effect up to level 5 (like the vanilla).
    
    Also, during a raid, once a tamed pony kills the last raider, the Hero of the Village advancement will be awarded to the player.

--- src/main/java/org/projectflawless/minelittleflawless/FlawlessEvents.java
+++ src/main/java/org/projectflawless/minelittleflawless/FlawlessEvents.java
@@ -1,9 +1,18 @@
 package org.projectflawless.minelittleflawless;
 
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.raid.Raid;
+import net.minecraft.world.entity.raid.Raider;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import org.projectflawless.minelittleflawless.entity.Flawless;
 import org.projectflawless.minelittleflawless.entity.TamableTamersPony;
 
@@ -21,6 +30,43 @@ public class FlawlessEvents {
                     entity.setLastHurtByPlayer(player);
             }
             return true;
+        });
+
+        // This allows tamed ponies to give the player bad omen if the raider they killed is a captain.
+        // It also allows them to give you the Hero of the Village.
+        ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register((serverLevel, entity, killedEntity) -> {
+            if (killedEntity instanceof Raider raider) {
+                if (entity instanceof TamableTamersPony tamersPony && tamersPony.isTame()) {
+                    if (tamersPony.getOwner() instanceof Player player) {
+                        Raid raid = serverLevel.getRaidAt(killedEntity.blockPosition());
+
+                        if (raid != null) {
+                            raid.addHeroOfTheVillage(player);
+                        }
+
+                        if (raider.isPatrolLeader() && raid == null && serverLevel.getRaidAt(raider.blockPosition()) == null) {
+                            ItemStack itemStack = raider.getItemBySlot(EquipmentSlot.HEAD);
+
+                            if (!itemStack.isEmpty() && ItemStack.matches(itemStack, Raid.getLeaderBannerInstance())) {
+                                MobEffectInstance mobEffectInstance = player.getEffect(MobEffects.BAD_OMEN);
+                                int i = 1;
+                                if (mobEffectInstance != null) {
+                                    i += mobEffectInstance.getAmplifier();
+                                    player.removeEffectNoUpdate(MobEffects.BAD_OMEN);
+                                } else {
+                                    i--;
+                                }
+
+                                i = Mth.clamp(i, 0, 4);
+                                MobEffectInstance mobEffectInstance2 = new MobEffectInstance(MobEffects.BAD_OMEN, 120000, i, false, false, true);
+                                if (!raider.level().getGameRules().getBoolean(GameRules.RULE_DISABLE_RAIDS)) {
+                                    player.addEffect(mobEffectInstance2);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         });
     }
 }
